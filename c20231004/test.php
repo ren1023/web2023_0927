533f139c9b3b57ce8160acca35e4c2b880352975
ren1023 <renyou.1023@gmail.com>
2023-10-04 22:23:53 +0800

    補資料

---
+++ c20231004/test.php
@@ -0,0 +1,32 @@
+<style>
+    /* h1 {
+        color : red;
+        text-align : center;
+    } */
+</style>
+<html>
+    <body>
+        <!-- 123
+        456     -->
+
+    </body>
+</html>
+
+<?php 
+    //php
+    // $myArr = [
+    //     's1' => 'amy',
+    //     's2' => 'bob',
+    //     's3' => 'cat',
+    // ];
+
+?>
+
+<!-- js -->
+<script>
+    // let myObj = {
+    //     s1 : 'amy',
+    //     s1 : 'bob',
+    //     s1 : 'cat',
+    // }
+</script>
