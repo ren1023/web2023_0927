eb036c94b2ec15a567ef96776ed2a2587133e498
ren1023 <renyou.1023@gmail.com>
2024-02-15 13:14:45 +0800

    使用ajax來串api資料

---
+++ c20240215/c20240215_03_php_back.php
@@ -0,0 +1,15 @@
+<?php
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: *");
+// header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+
+$data=[
+    's1'=>'amy',
+    's2'=>'bob',
+    's3'=>'cat'
+];
+
+echo json_encode($data);
+
+
+?>
